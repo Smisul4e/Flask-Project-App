b6e5f74cfca6392d3f9193bbcb7d8693a0b9cd08
Smisul4e <teo.plamka@gmail.com>
2024-06-10 06:47:37 +0200

    add new dictionary 'users' to display user information

--- main.py
+++ main.py
@@ -3,15 +3,24 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+users = {
+    'Alice': {'age': 25, 'country': 'USA'},
+    'Bob': {'age': 30, 'country': 'UK'},
+    'Charlie': {'age': 35, 'country': 'Australia'}
+}
+
+
 @app.route('/')
 def home():
-    name = request.args.get('name', 'Alice')  # Default to 'Alice' if no name is provided
+    name = request.args.get('name', 'Alice')
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return render_template('index.html', title='Home Page', name=name, current_time=current_time)
+    return render_template('index.html', title='Home Page', name=name, current_time=current_time, users=users)
+
 
 @app.route('/form')
 def form():
     return render_template('form.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
