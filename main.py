91d86b204a41ca2f0c8547676352af7ad84b6381
Smisul4e <teo.plamka@gmail.com>
2024-06-11 10:49:15 +0200

    add css file and static folder

--- main.py
+++ main.py
@@ -3,23 +3,27 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+# Примерен речник с потребители
 users = {
     'Alice': {'age': 25, 'country': 'USA'},
     'Bob': {'age': 30, 'country': 'UK'},
     'Charlie': {'age': 35, 'country': 'Australia'}
 }
 
+# Конфигуриране на Flask да зарежда статичните файлове от директорията "static"
+app.static_folder = 'static'
+
 
 @app.route('/')
-def home():
-    name = request.args.get('name', 'Alice')
+def index():
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    name = request.args.get('name', 'Plamena')
     return render_template('index.html', title='Home Page', name=name, current_time=current_time, users=users)
 
 
 @app.route('/form')
 def form():
-    return render_template('form.html')
+    return render_template('form.html', title='Form Page')
 
 
 @app.route('/all-users')
