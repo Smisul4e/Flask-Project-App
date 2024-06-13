71f150de84a0bf8dab1fef1125e90421f083c7de
Smisul4e <teo.plamka@gmail.com>
2024-06-13 08:36:43 +0200

    new file greet.html

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+
 # Примерен речник с потребители
 users = {
     'Alice': {'age': 25, 'country': 'USA'},
@@ -10,23 +11,28 @@ users = {
     'Charlie': {'age': 35, 'country': 'Australia'}
 }
 
+
 @app.route('/')
 def index():
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    name = request.args.get('name', 'Guest')
+    name = request.args.get('name', 'Plamena')
     return render_template('index.html', title='Home Page', name=name, current_time=current_time, users=users)
+
 
 @app.route('/form')
 def form():
     return render_template('form.html', title='Form Page')
 
+
 @app.route('/all-users')
 def all_users():
     return render_template('all_users.html', title='All Users', users=users)
 
+
 @app.route('/greet/<name>')
 def greet(name):
-    return render_template('index.html', title='Greet Page', name=name, users=users)
+    return render_template('greet.html', title='Greet Page', name=name)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
