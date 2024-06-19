5b28e5f3e43b35685143f22d321e34d3ece03f89
Smisul4e <teo.plamka@gmail.com>
2024-06-19 20:50:42 +0200

    add image nad new folder

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import logging
 from flask import Flask, render_template, request, redirect, url_for
 from datetime import datetime
 
 app = Flask(__name__)
 
-# Примерен речник с потребители
+# Logging configuration
+logging.basicConfig(level=logging.DEBUG)
+
+# Sample dictionary of users
 users = {
     'Alice': {'age': 25, 'country': 'USA'},
     'Bob': {'age': 30, 'country': 'UK'},
@@ -14,7 +18,11 @@ users = {
 def index():
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     name = request.args.get('name', 'Guest')
-    return f"Welcome to the home page, {name}! Current time: {current_time}"
+    return render_template('index.html', title='Home Page', name=name, current_time=current_time)
+
+@app.route('/all-users')
+def all_users():
+    return render_template('all_users.html', title='All Users', users=users)
 
 @app.route('/update-country', methods=['GET', 'POST'])
 def update_country():
@@ -23,22 +31,10 @@ def update_country():
         country = request.form['country']
         if name in users:
             users[name]['country'] = country
-            return redirect(url_for('all_users'))  # Redirect to the all_users route
+            return redirect(url_for('all_users'))
         else:
             return f"User {name} not found."
-    return '''
-        <form method="post">
-            <label for="name">Username:</label><br>
-            <input type="text" id="name" name="name"><br><br>
-            <label for="country">New Country:</label><br>
-            <input type="text" id="country" name="country"><br><br>
-            <input type="submit" value="Update Country">
-        </form>
-    '''
-
-@app.route('/all-users')
-def all_users():
-    return render_template('all_users.html', title='All Users', users=users)
+    return render_template('update_country.html', title='Update Country')
 
 if __name__ == '__main__':
     app.run(debug=True)
